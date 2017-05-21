d650c4983f8efe2a7cfe71e7bc7dfbcb1c537e9e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-05-21 12:59:05 -0300

    roomservice: Change default branch to nougat

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -43,7 +43,7 @@ DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
 custom_local_manifest = ".repo/local_manifests/viper_manifest.xml"
-custom_default_revision = "n-mr1"
+custom_default_revision = "nougat"
 custom_dependencies = "viper.dependencies"
 org_manifest = "viperD"  # leave empty if org is provided in manifest
 org_display = "Viper0S-Devices"  # needed for displaying
