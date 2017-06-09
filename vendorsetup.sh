cc72e127081e3976e3e15d2395361777c996aa2d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-06-09 19:03:01 -0300

    Get devices from repository

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in $(cat vendor/viper/viper.devices)
+for device in $(curl -s https://raw.githubusercontent.com/ViperOS-Project/devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\":".*?"' | sed -e 's/codename//;s/\"//g;s/\://')
 do
 add_lunch_combo viper_$device-userdebug
 done
