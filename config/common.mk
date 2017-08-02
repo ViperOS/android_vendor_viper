910a034190d97e558bed3e5f26bec82e684ec09b
Anderson Mendes <lge500meu@gmail.com>
2017-08-02 01:20:47 +0000

    Bump to v3.0 Coral

--- config/common.mk
+++ config/common.mk
@@ -263,8 +263,8 @@ DEVICE_PACKAGE_OVERLAYS += vendor/viper/overlay/common
 
 # Versioning System
 # ViperOs version.
-VIPER_VERSION_CODENAME := Python
-VIPER_VERSION_NUMBER := v2.1
+VIPER_VERSION_CODENAME := Coral
+VIPER_VERSION_NUMBER := v3.0
 
 VIPER_DEVICE := $(VIPER_BUILD)
 
