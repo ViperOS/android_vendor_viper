8866958b10bc6cbd35f8c65b13d1c5d2ed134ef3
JBCDroid2 <jonathan.lyndo@outlook.com>
2019-03-07 00:41:40 -0300

    generate_json_build_info: Add to generate Unix Timestamp

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -2,10 +2,11 @@
 if [ "$1" ]
 then
   file_path=$1
-  file_name=$(basename "$file_path")
+  datetime=$(date +%s)
+  filename=$(basename "$file_path")
   if [ -f $file_path ]; then
-    file_size=$(stat --printf="%s" "$file_path")
-    md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    echo "{\n   \"file_name\":\"$file_name\",\n   \"file_size\":$file_size,\n   \"md5\":\"$md5\"\n}" > $file_path.json
+    size=$(stat --printf="%s" "$file_path")
+    id=$(cat "$file_path.md5sum" | cut -d' ' -f1)
+    echo "{\n   \"datetime\":\"$datetime\",\n   \"filename\":\"$filename\",\n   \"size\":$size,\n   \"id\":\"$id\"\n}" > $file_path.json
   fi
 fi
