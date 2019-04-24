AfterlifeOS/vendor_afterlife

config: Include old AOSP alarms/notifications/ringtones
Change-Id: I8546d20098a0d117c5349dbeb73cfe1e72030990

config/common.mk
@@ -96,6 +96,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-power-whitelist.xml:system/etc/sysconfig/lineage-power-whitelist.xml
 
+# Include AOSP audio files
+include vendor/lineage/config/aosp_audio.mk
+
 # Include Lineage audio files
 include vendor/lineage/config/lineage_audio.mk
 

ViperOS/android_vendor_viper

config/common.mk
@@ -110,6 +110,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/viper/config/permissions/lineage-power-whitelist.xml:system/etc/sysconfig/lineage-power-whitelist.xml
 
+# Include AOSP audio files
+include vendor/viper/config/aosp_audio.mk
+
 # Include Lineage audio files
 include vendor/viper/config/lineage_audio.mk
 